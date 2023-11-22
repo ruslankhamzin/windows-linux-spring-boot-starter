524714a2de67d2d1bb22464215efedb3f6d04b27
Ruslan Khamzin <74431178+ruslankhamzin@users.noreply.github.com>
2023-11-22 10:52:26 +0400

    init commit

---
+++ src/main/java/com/example/starterforwindowsandlinux/StarterForWindowsAndLinuxApplication.java
@@ -0,0 +1,13 @@
+package com.example.starterforwindowsandlinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterForWindowsAndLinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarterForWindowsAndLinuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/starterforwindowsandlinux/StarterForWindowsAndLinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.starterforwindowsandlinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterForWindowsAndLinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
